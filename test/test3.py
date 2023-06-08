7b8f5fbdd60bb4fb24604e4478486ec0c5e99a8f
master1018 <1764924903@qq.com>
2023-06-08 17:19:48 +0800

    LOG:add test code

---
+++ test/test3.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import time
+
+progress_text = "Operation in progress. Please wait."
+my_bar = st.progress(0, text=progress_text)
+
+for percent_complete in range(100):
+    time.sleep(0.1)
+    my_bar.progress(percent_complete + 1, text=progress_text)
